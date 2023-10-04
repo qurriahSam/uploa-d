68ee348f3f8c5b0c822ffb108ab9ca7d9de7f3bb
qurriahSam <mugirethe1@gmail.com>
2023-10-04 19:20:56 +0300

    feat: add components and directives to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { CardComponent } from './components/card/card.component';
+import { ImageUploaderDirective } from './directives/image-uploader.directive';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, CardComponent, ImageUploaderDirective],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
