47fc0d9750ee08399d300e5a35d1a4d6b41b2668
qurriahSam <mugirethe1@gmail.com>
2023-10-10 21:43:53 +0300

    feat(image-uploader.directive): enable drag n drop

--- src/app/directives/image-uploader.directive.ts
+++ src/app/directives/image-uploader.directive.ts
@@ -7,12 +7,50 @@ import {
 } from '@angular/core';
 import { ImageFile } from '../model/image-file';
 
+enum DropColor {
+  Default = '#C6E4F1', // Default color
+  Over = '#ACADAD', // Color to be used once the file is "over" the drop box
+}
+
 @Directive({
   selector: '[appImageUploader]',
 })
 export class ImageUploaderDirective {
-  @Output() dropFiles: EventEmitter<ImageFile> = new EventEmitter();
-  @HostBinding('style.background') backgroundColor;
+  @Output() dropFile: EventEmitter<ImageFile> = new EventEmitter();
+  @HostBinding('style.background') backgroundColor = DropColor.Default;
+
+  @HostListener('dragover', ['$event']) public dragOver(event: DragEvent) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.backgroundColor = DropColor.Over;
+  }
+
+  @HostListener('dragleave', ['$event']) public dragLeave(event: DragEvent) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.backgroundColor = DropColor.Default;
+  }
+
+  @HostListener('drop', ['$event']) public drop(e: DragEvent) {
+    e.preventDefault();
+    e.stopPropagation();
+    this.backgroundColor = DropColor.Default;
+
+    let fileList = e.dataTransfer?.files[0];
+    let imgFile: ImageFile = {
+      file: undefined,
+      url: '',
+    };
+
+    if (fileList != undefined) {
+      imgFile.file = fileList;
+      imgFile.url = window.URL.createObjectURL(imgFile.file);
+    }
+
+    if (imgFile.file != undefined) {
+      this.dropFile.emit(imgFile);
+    }
+  }
 
   constructor() {}
 }
