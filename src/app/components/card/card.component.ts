e3eecf05124cd0767f79fc7b06131f1c9161f516
qurriahSam <mugirethe1@gmail.com>
2023-10-10 21:48:15 +0300

    feat(card-component): display image on drop

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { ImageFile } from 'src/app/model/image-file';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.scss'],
 })
 export class CardComponent {
+  file: ImageFile | undefined;
 
+  onDropFile(file: ImageFile): void {
+    this.file = file;
+  }
 }
