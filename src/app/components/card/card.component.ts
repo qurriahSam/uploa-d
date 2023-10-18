3b2cf2ac4f7ae72edc0eddc1c216401d4eae4e1c
qurriahSam <mugirethe1@gmail.com>
2023-10-18 20:33:45 +0300

    feat: enable upload from button click

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -12,4 +12,15 @@ export class CardComponent {
   onDropFile(file: ImageFile): void {
     this.file = file;
   }
+
+  onFileSelected(e: any): void {
+    const file = e.target.files[0];
+    if (file) {
+      this.file = {
+        file: file,
+        url: window.URL.createObjectURL(file),
+      };
+      console.log(this.file.url);
+    }
+  }
 }
