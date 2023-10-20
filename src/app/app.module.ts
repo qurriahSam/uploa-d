2e2c855b5ebc4f420a1b5a3fdccb814e604686f0
qurriahSam <mugirethe1@gmail.com>
2023-10-20 18:44:04 +0300

    style: replace progress spinner with bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatIconModule } from '@angular/material/icon';
 
 import { CardComponent } from './components/card/card.component';
 import { ImageUploaderDirective } from './directives/image-uploader.directive';
@@ -24,7 +25,8 @@ import { ImageUploaderDirective } from './directives/image-uploader.directive';
     AngularFireStorageModule,
     MatCardModule,
     MatButtonModule,
-    MatProgressSpinnerModule,
+    MatProgressBarModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
